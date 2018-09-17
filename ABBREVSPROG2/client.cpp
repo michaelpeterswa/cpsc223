828be859d5020375faef5693b71b911f3c6c49e5
Michael Peters <michaelpeterswa@icloud.com>
2018-09-17 00:23:32 -0700

    added homework 2

---
+++ ABBREVSPROG2/client.cpp
@@ -0,0 +1,179 @@
+/*
+Michael Peters and Nathan Flack
+CPSC223 01
+Homework Assignment 1
+Thursday, September 14, 2018
+
+ADT Item (Texting Abbreviation and Meaning)
+Specification file for ADT Item (Testing Abbreviation and Meaning)
+   data object: a key or texting abbreviation and meaning (unique)
+   operations: create an empty Item, destroy an Item
+               read an Item from a file, write an Item to a file
+               read an Item from the user, write an Item to the screen,
+               convert an Item to a number (for what's coming in Dictionary)
+
+filename: client.cpp
+
+This program, client.cpp is designed to display the functions of the Item Class.
+
+*/
+
+#include "item.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+  ifstream infile("in.dat"); //file input used for testing reading from file
+  ofstream outfile("out.dat"); //file output used for testing writing to a file
+  Item firstOne, thirdOne, fromFile;
+  Key firstKey, fromFileKey;
+
+  cout << "-----------------------" << endl;
+  cout << "  INITIALIZE OBJECTS" << endl;
+  cout << endl;
+  cout << "created four Item objects (firstOne, thirdOne, fromFile)" << endl;
+  cout << "created two Key objects (firstKey, fromFileKey)" << endl;
+  cout << endl;
+
+  cout << "-----------------------" << endl;
+  cout << "  CLASS KEY TESTING" << endl;
+  cout << endl;
+
+  cout << "Enter a texting abbreviation for firstKey -> ";
+  cin >> firstKey;
+  cout << endl;
+  cout << "reading from in.dat..." << endl;
+  infile >> fromFileKey;
+  cout << endl;
+  cout << "fromFileKey -- " << fromFileKey << endl;
+  cout << endl;
+
+  cout << "writing firstKey to out.dat..." << endl;
+  cout << endl;
+  outfile << "firstKey -> " << firstKey << endl;
+
+
+  cout << "testing class Key copy constructor..." << endl;
+  cout << "copying firstKey to secondKey..." << endl;
+  Key secondKey(firstKey);
+  cout << endl;
+
+  cout << "firstKey -- " << firstKey << endl;
+  cout << "secondKey -- " << secondKey << endl;
+  cout << endl;
+
+  cout << "testing convertToInteger on Key firstKey..." << endl;
+  cout << "result: " << firstKey.convertToInteger() << endl;
+  cout << endl;
+
+  cout << "testing == operator on firstKey and fromFileKey..." << endl;
+    if(firstKey == fromFileKey){
+      cout << "firstKey and fromFileKey match" << endl;
+    }
+    else
+      cout << "firstKey and fromFileKey do not match" << endl;
+      cout << endl;
+
+  cout << "testing == operator on firstKey and secondKey..." << endl;
+    if(firstKey == secondKey){
+      cout << "firstKey and secondKey match" << endl;
+    }
+    else
+      cout << "firstKey and secondKey do not match" << endl;
+      cout << endl;
+
+  cout << "testing < operator on firstKey and fromFileKey..." << endl;
+    if(firstKey < fromFileKey){
+      cout << "firstKey is less than fromFileKey" << endl;
+    }
+    else
+      cout << "firstKey is not less than fromFileKey" << endl;
+      cout << endl;
+
+  cout << "testing < operator on firstKey and secondKey..." << endl;
+    if(firstKey < secondKey){
+      cout << "firstKey is less than secondKey" << endl;
+    }
+    else
+      cout << "firstKey is not less than secondKey" << endl;
+      cout << endl;
+
+  cout << "-----------------------" << endl;
+  cout << "  CLASS ITEM TESTING" << endl;
+  cout << endl;
+
+  cout << "Enter a texting abbreviation, and it's associated meaning -> ";
+  cin >> firstOne;
+  cout << endl;
+  cout << "reading from in.dat..." << endl;
+  infile >> fromFile;
+  cout << endl;
+
+  cout << "firstOne -- " << firstOne;
+  cout << "fromFile -- " << fromFile;
+  cout << endl;
+
+  cout << "writing firstOne to out.dat..." << endl;
+  cout << endl;
+  outfile << "firstOne -> " << firstOne << endl;
+
+  cout << "testing class Item copy constructor..." << endl;
+  cout << "copying firstKey to secondKey..." << endl;
+  Item secondOne(firstOne);
+  cout << endl;
+  cout << "firstOne -- " << firstOne;
+  cout << "secondOne -- " << secondOne;
+  cout << endl;
+
+  cout << "testing cascading equals operator..." << endl;
+  cout << "copying firstOne to secondOne and thirdOne..." << endl;
+  cout << endl;
+
+  thirdOne = secondOne = firstOne;
+
+  cout << "firstOne -- " << firstOne;
+  cout << "secondOne -- " << secondOne;
+  cout << "thirdOne -- " << thirdOne;
+  cout << endl;
+
+  cout << "testing convertToInteger on Item firstOne..." << endl;
+  cout << "result: " << firstOne.convertToInteger() << endl;
+  cout << endl;
+
+  cout << "testing == operator on firstOne and fromFile..." << endl;
+    if(firstOne == fromFile){
+      cout << "firstKey and fromFileKey match" << endl;
+    }
+    else
+      cout << "firstKey and fromFileKey do not match" << endl;
+      cout << endl;
+
+  cout << "testing == operator on firstOne and secondOne..." << endl;
+    if(firstOne == secondOne){
+      cout << "firstOne and secondOne match" << endl;
+    }
+    else
+      cout << "firstOne and secondOne do not match" << endl;
+      cout << endl;
+
+  cout << "testing < operator on firstOne and fromFile..." << endl;
+    if(firstOne < fromFile){
+      cout << "firstOne is less than fromFile" << endl;
+    }
+    else
+      cout << "firstOne is not less than fromFile" << endl;
+      cout << endl;
+
+  cout << "testing < operator on firstOne and secondOne..." << endl;
+    if(firstOne < secondOne){
+      cout << "firstOne is less than secondOne" << endl;
+    }
+    else
+      cout << "firstOne is not less than secondOne" << endl;
+      cout << endl;
+      cout << endl;
+
+  return 0;
+}
---
+++ ABBREVSPROG2/item.cpp
@@ -0,0 +1,66 @@
+#include "item.h"
+
+//allows for input of an item from either the standard input device or a file
+//pre input has been opened and readied for the input of
+//    a texting abbreviation followed by newline and associated meaning with
+//    spaces between words ending with a newline. For example:
+//    lol
+//    laugh out loud
+//
+//post rightHandSideItem has been filled with a texting abbreviation and its
+//     associated meaning
+//usage cin >> myItem;
+istream& operator>> (istream& input, Item& rightHandSideItem){
+  char newline;
+
+  input >> rightHandSideItem.textingAbbreviation;
+  input.get(newline);
+  getline(input,rightHandSideItem.textingMeaning);
+}
+
+//allows for the output of an item to the standard output device or a file
+//pre rightHandSideItem has been filled with a texting abbreviation and
+//    its associated meaning
+//    output has been opened
+//post rightHandSideItem has been written to the output device
+//     lol:  laugh out loud
+//
+//usage  outfile << myItem;
+ostream& operator<< (ostream& output, const Item& rightHandSideItem){
+  output << rightHandSideItem.textingAbbreviation << ": " << rightHandSideItem.textingMeaning << endl;
+}
+
+//creates an empty item (texting abbreviation and its associated meaning)
+//post
+//usage Item aitem;
+Item::Item(){
+
+}
+
+//releases memory for a texting abbreviation and its associated meaning
+//pre Item object exists
+//post Item object no longer exists, memory has been freed
+//usage automatically done at the end of scope
+Item::~Item(){
+
+}
+
+//creates a new item and copies the right hand side item into item
+//pre
+//post
+//usage  Item myItem(yourItem);
+Item::Item(const Item& rightHandSideItem){
+  textingMeaning = rightHandSideItem.textingMeaning;
+  textingAbbreviation = rightHandSideItem.textingAbbreviation;
+}
+
+//copies rightHandSideItem's member variables into an existing item
+//pre Item object exists
+//post Item object contains the member variables from rightHandSideItem
+//usage exampleItem = secondItem;
+Item& Item::operator=(const Item& rightHandSideItem){
+  if(this != &rightHandSideItem)
+    textingAbbreviation = rightHandSideItem.textingAbbreviation;
+    textingMeaning = rightHandSideItem.textingMeaning;
+  return *this;
+}
